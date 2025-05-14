e4a63ae47f1a47966d1f2c3d4d6d59adec0370b4
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2025-05-13 22:48:58 -0300

    Update prova.hs

--- prova.hs
+++ prova.hs
@@ -1,25 +1,22 @@
-import System.Win32 (COORD(yPos))
-foo a b = if a == b then b else if a > b then foo (a-b) b else foo a (b-a)
+import Data.Time.Format.ISO8601 (yearFormat)
+primo n = prim0 n (n-1)
+prim0 n 1 = True
+prim0 n d = if rem n d == 0 then False else prim0 n (d-1)
+
+primod = [(x)|x <- [2..1000],primo x]
+numero = [n|n<- [1..10000]]
+
 
 impares [] = []
-impares (x:xs) = if rem x 2 /= 0 then x:impares xs else impares xs 
+impares (x:xs) = if rem x 2 == 0 then impares xs else x:impares xs
 
 pares [] = []
 pares (x:xs) = if rem x 2 == 0 then x:pares xs else pares xs
 
-remover n [] = []
-remover n (x:xs) = if x == n then remover n xs else x:remover n xs
-
-removerum n [] = []
-removerum n (x:xs) = if n==x then xs else x:removerum n xs
-
-todos [] = True
-todos (x:xs) = if x == False then False else todos xs
+primeiro (x,y) = x
+primeiros [] = []
+primeiros (x:xs) = primeiro x:primeiros xs
 
 segundo (x,y) = y
 segundos [] = []
 segundos (x:xs) = segundo x:segundos xs
-
-primeiro (x,y) = x
-primeiros [] = []
-primeiros (x:xs) = primeiro x:primeiros xs
